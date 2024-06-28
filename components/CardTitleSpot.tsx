2a875945c37d62cf8c4770353b1ad8a53db25711
Vaisakh <vaisakhpn78@gmail.com>
2024-06-28 20:15:44 +0530

    card titles modified

--- components/CardTitleSpot.tsx
+++ components/CardTitleSpot.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const CardTitleSpot = () => {
   return (
     <div className="flex flex-col p-2">
-      <h1 className="text-3xl font-bold p-3">
-        Best Spots in <span className="text-red-500">Kochi</span>
-      </h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold p-3">
+          Best Spots in <span className="text-red-500">Kochi</span>
+        </h1>
+        <Link href="#" className="text-[18px] text-blue-500 font-semibold p-4">
+          more...
+        </Link>
+      </div>
+
       <div className="border-t  border-gray-300 mb-2 " />
     </div>
   );

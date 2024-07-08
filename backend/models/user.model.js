0ffd9a1a81abfc061974712a176e83ed7a38e228
Vaisakh <vaisakhpn78@gmail.com>
2024-07-08 15:29:16 +0530

    user model created

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -21,14 +21,19 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    confirmpassword: {
-      type: String,
-      required: true,
-    },
     avatar: {
       type: String,
-      default:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    subscribedUsers: {
+      type: [String],
+    },
+    fromGoogle: {
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true }

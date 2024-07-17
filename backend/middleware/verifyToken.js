823a2f59b822de8098d53fc1c3fd856433fbacff
Vaisakh <vaisakhpn78@gmail.com>
2024-07-17 12:57:13 +0530

    verify token added

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,12 +1,18 @@
 import jwt from "jsonwebtoken";
-import { createError } from "./error.js";
+import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) return next(createError(401, "You are not authenticated!"));
+  if (!token) {
+    console.log("No token provided");
+    return next(errorHandler(401, "You are not authenticated!"));
+  }
 
-  jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Token is not valid!"));
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log("Token verification failed:", err);
+      return next(errorHandler(403, "Token is not valid!"));
+    }
     req.user = user;
     next();
   });

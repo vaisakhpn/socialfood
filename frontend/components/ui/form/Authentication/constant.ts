216186b51f71dee86940e8eba92017738710be9f
Vaisakh <vaisakhpn78@gmail.com>
2024-08-05 10:20:48 +0530

    auth schema modified

--- frontend/components/ui/form/Authentication/constant.ts
+++ frontend/components/ui/form/Authentication/constant.ts
@@ -1,22 +1,42 @@
 import { z } from "zod";
 
-const schema = z
-  .object({
-    name: z.string().nonempty("Name is required"),
-    phone: z.string().nonempty("Phone number is required"),
-    email: z
-      .string()
-      .email("Invalid email format")
-      .nonempty("Email is required"),
-    password: z
-      .string()
-      .min(6, "Password must be at least 6 characters")
-      .nonempty("Password is required"),
-    confirmPassword: z.string().nonempty("Confirm Password is required"),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords must match",
-    path: ["confirmPassword"], // Set this path to highlight the confirmPassword field
-  });
+const schema = (type: string) =>
+  z
+    .object({
+      username:
+        type === "sign-in"
+          ? z.string().optional()
+          : z
+              .string()
+              .min(3, "Minimum 3 characters are required")
+              .nonempty("Name is required"),
+      phoneNumber:
+        type === "sign-in"
+          ? z.string().optional()
+          : z
+              .string()
+              .min(10, "10 numbers are required")
+              .nonempty("Phone number is required"),
+      email: z
+        .string()
+        .email("Invalid email format")
+        .nonempty("Email is required"),
+      password: z
+        .string()
+        .min(6, "Password must be at least 6 characters")
+        .nonempty("Password is required"),
+      confirmPassword:
+        type === "sign-in"
+          ? z.string().optional()
+          : z.string().nonempty("Confirm Password is required"),
+    })
+    .refine(
+      (data) =>
+        type === "sign-up" ? data.password === data.confirmPassword : true,
+      {
+        message: "Passwords must match",
+        path: ["confirmPassword"],
+      }
+    );
 
 export default schema;
